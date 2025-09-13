03cf36967c8cfe798609e0d8a4f31552557eb3aa
Jay Banyer <jbanyer@gmail.com>
2025-09-13 15:12:01 +1000

    remove everything except TCP support

--- ModbusRTU.d.ts
+++ ModbusRTU.d.ts
@@ -1,12 +1,9 @@
 import { Socket, SocketConstructorOpts, TcpSocketConnectOpts } from 'net';
 import { TestPort } from "./TestPort";
-import { PortInfo } from '@serialport/bindings-cpp';
 
 export class ModbusRTU {
   constructor(port?: any);
   static TestPort: typeof TestPort
-
-  static getPorts(): Promise<PortInfo[]>
 
   open(callback?: Function): void;
   close(callback?: Function): void;
@@ -24,30 +21,14 @@ export class ModbusRTU {
   writeFC22(address: number, dataAddress: number, andMask: number, orMask: number, next: NodeStyleCallback<WriteMaskRegisterResult>): void;
 
   // Connection shorthand API
-  connectRTU(path: string, options: SerialPortOptions, next: Function): void;
-  connectRTU(path: string, options: SerialPortOptions): Promise<void>;
   connectTCP(ip: string, options: TcpPortOptions, next: Function): void;
   connectTCP(ip: string, options: TcpPortOptions): Promise<void>;
-  connectUDP(ip: string, options: UdpPortOptions, next: Function): void;
-  connectUDP(ip: string, options: UdpPortOptions): Promise<void>;
   connectTcpRTUBuffered(ip: string, options: TcpRTUPortOptions, next: Function): void;
   connectTcpRTUBuffered(ip: string, options: TcpRTUPortOptions): Promise<void>;
-  connectTelnet(ip: string, options: TelnetPortOptions, next: Function): void;
-  connectTelnet(ip: string, options: TelnetPortOptions): Promise<void>;
-  connectC701(ip: string, options: C701PortOptions, next: Function): void;
-  connectC701(ip: string, options: C701PortOptions): Promise<void>;
-  connectRTUBuffered(path: string, options: SerialPortOptions, next: Function): void;
-  connectRTUBuffered(path: string, options: SerialPortOptions): Promise<void>;
-  connectAsciiSerial(path: string, options: SerialPortOptions, next: Function): void;
-  connectAsciiSerial(path: string, options: SerialPortOptions): Promise<void>;
   linkTCP(socket: Socket, options: TcpPortOptions, next: Function): void;
   linkTCP(socket: Socket, options: TcpPortOptions): Promise<void>;
   linkTcpRTUBuffered(socket: Socket, options: TcpRTUPortOptions, next: Function): void;
   linkTcpRTUBuffered(socket: Socket, options: TcpRTUPortOptions): Promise<void>;
-  linkTelnet(socket: Socket, options: TelnetPortOptions, next: Function): void;
-  linkTelnet(socket: Socket, options: TelnetPortOptions): Promise<void>;
-  connectRTUSocket(socket: Socket, next: Function): void;
-  connectRTUSocket(socket: Socket): Promise<void>;
 
   // Promise API
   setID(id: number): void;
